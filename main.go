3865fe211093a40eedbf29e550e2ef8694a8e9cc
iantalo0 <talooyweka@gmail.com>
2026-04-16 16:30:37 +0300

    Commond Line Text

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len (os.Args) != 3 {
+		fmt.Println("Usage: go run . input.txt output. txt")
+	return
+    }
+
+    inputFile := os.Args[1]
+    outputFile := os.Args[2]
+
+    content, err := os.ReadFile(inputFile)
+    if err != nil {
+	fmt.Println(err)
+	return 
+    }
+
+    result := processText(string(content))
+
+    err = os.WriteFile(outputFile, []byte(result), 0644)
+    if err != nil {
+	    fmt.Println(err)
+	    return
+    }
+}
+
+func processText(text string) string {
+	return text
+}
